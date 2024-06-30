c4987ca4e5510b2de74e04dbe6d0f16dac9c6d4c
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2024-06-30 19:36:00 -0400

    mejorar version

--- src/main/java/org/example/ordering/BubbleSort.java
+++ src/main/java/org/example/ordering/BubbleSort.java
@@ -9,7 +9,7 @@ public final class BubbleSort implements Function<List<Integer>, Void> {
         for (var i = 0; i < data.size() - 1; i++) {
             var breakCount = 0;
             for (var j = 0; j < data.size() - i - 1; j++) {
-                if (data.get(j) > data.get(j + 1) ) {
+                if (data.get(j) > data.get(j + 1)) {
                     this.swap(j, j + 1, data);
                     breakCount++;
                 }
@@ -24,7 +24,7 @@ public final class BubbleSort implements Function<List<Integer>, Void> {
     private void swap(Integer first, Integer second, List<Integer> data) {
         var value1 = data.get(first);
         var value2 = data.get(second);
-        var temp = value1;
+        final var temp = value1;
         value1 = value2;
         value2 = temp;
         data.set(first, value1);
